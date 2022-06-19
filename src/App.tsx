ac6a67e63e2c80f8f4e4860b936aa47eddf05a60
heyjul3s <julian.cy.low@gmail.com>
2022-06-19 17:00:27 +1000

    App Root update - update ReactDOM render invocation - add StrictMode to App - pass <Component /> to element prop for AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
+import { HelmetProvider } from 'react-helmet-async';
+
+import { configureAppStore } from '@/store/store';
 import { AppRoutes } from './AppRoutes';
+import { theme } from '@/theme';
 
 export function App() {
+  const store = configureAppStore();
+
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <HelmetProvider>
+          <ChakraProvider theme={theme}>
+            <AppRoutes />
+          </ChakraProvider>
+        </HelmetProvider>
+      </Provider>
+    </React.StrictMode>
   );
 }
--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Error404, Home } from '@/pages';
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={Home} />
-      <Route path="*" element={Error404} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
   );
 }
