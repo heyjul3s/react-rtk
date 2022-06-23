e8c6a96e9510612b03e89c728d1966c2fcd53f22
heyjul3s <julian.cy.low@gmail.com>
2022-06-23 15:34:11 +1000

    lint fix for theme file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
-import { colors, fonts } from './foundations/index';
 import { components } from './components';
+import { colors, fonts } from './foundations/index';
 
 export const theme = extendTheme({
   colors,
