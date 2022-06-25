4a94eb201d4464d6982fe083d5390bb9c9fb40b8
heyjul3s <julian.cy.low@gmail.com>
2022-06-25 14:39:31 +1000

    initial basic RTKQ implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
-import { configureAppStore } from '@/store/store';
+import { store } from '@/store/store';
 import { theme } from '@/theme';
 import { AppRoutes } from './AppRoutes';
 
 export function App() {
-  const store = configureAppStore();
-
   return (
     <React.StrictMode>
       <Provider store={store}>
